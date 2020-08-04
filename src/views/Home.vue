a6076f3b7b9c385259199e8a1aa8bfad67578338
Mark <markandrewhamm@gmail.com>
2020-08-03 20:56:17 -0500

    initial commit, index and create action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <button v-on:click="addProduct">Add Product</button>
+    <h1>{{ name }}</h1>
+
+    <div v-for="product in products">
+      <p>name: {{ product.name }}</p>
+      <p>price: {{ product.price }}</p>
+      <!-- <p>image_url: {{ product.image_url }}</p> -->
+      <img v-bind:src="product.image_url">
+      <hr>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      name: "Mark",
+      products: [],
+    };
+  },
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      console.log("This is the Products Index");
+      axios.get("api/products").then((response) => {
+        console.log(response);
+        this.products = response.data;
+      });
+    },
+    addProduct: function () {
+      console.log("Adding products now...");
+
+      var params = {
+        name: "Applesauce",
+        description: "saucy apples",
+        price: 2,
+        image_url:
+          "https://st.depositphotos.com/1027198/4199/i/450/depositphotos_41993527-stock-photo-bowl-of-apple-sauce.jpg",
+      };
+
+      axios.post("api/products", params).then((response) => {
+        console.log(response.data);
+        this.products.push(response.data);
+      });
+    },
+  },
+};
 </script>
