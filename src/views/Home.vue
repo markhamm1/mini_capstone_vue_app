e0984a422ac5f65bc1971cc13b7cb14505045f43
Mark <markandrewhamm@gmail.com>
2020-08-04 20:05:55 -0500

    show action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <p>Name: <input v-model="newProductName" type="text"></p>
+    <p>Description: <input v-model="newProductDescription" type="text"></p>
+    <p>Price: <input v-model="newProductPrice" type="text"></p>
+    <p>Image Url: <input v-model="newProductImageUrl" type="text"></p>
     <button v-on:click="addProduct">Add Product</button>
-    <h1>{{ name }}</h1>
+    <!-- <h1>{{ name }}</h1> -->
 
     <div v-for="product in products">
       <p>name: {{ product.name }}</p>
       <p>price: {{ product.price }}</p>
       <!-- <p>image_url: {{ product.image_url }}</p> -->
-      <img v-bind:src="product.image_url">
+      <img v-bind:src="product.image_url" style="width:300px;">
+      <br>
+      <button v-on:click="showInfo(product)">Show Info</button>
       <hr>
     </div>
+
+    <dialog id="product-details">
+      <form method="dialog">
+        <h1>{{ currentProduct.name }}: info</h1>
+        <p>{{ currentProduct.description }}</p>
+        <p>Price: {{ currentProduct.price }}</p>
+        <img v-bind:src="currentProduct.image_url" style="width:600px;">
+        <button>Close</button>
+      </form>
+    </dialog>
+
   </div>
 </template>
 
@@ -25,6 +42,11 @@ export default {
       message: "Welcome to Vue.js!",
       name: "Mark",
       products: [],
+      newProductName: "",
+      newProductDescription: "",
+      newProductPrice: "",
+      newProductImageUrl: "",
+      currentProduct: {},
     };
   },
   created: function () {
@@ -41,18 +63,32 @@ export default {
     addProduct: function () {
       console.log("Adding products now...");
 
+      // console.log(this.newProductName);
+
       var params = {
-        name: "Applesauce",
-        description: "saucy apples",
-        price: 2,
-        image_url:
-          "https://st.depositphotos.com/1027198/4199/i/450/depositphotos_41993527-stock-photo-bowl-of-apple-sauce.jpg",
+        name: this.newProductName,
+        description: this.newProductDescription,
+        price: this.newProductPrice,
+        image_url: this.newProductImageUrl,
       };
 
-      axios.post("api/products", params).then((response) => {
-        console.log(response.data);
-        this.products.push(response.data);
-      });
+      axios
+        .post("api/products", params)
+        .then((response) => {
+          console.log(response.data);
+          this.products.push(response.data);
+        })
+        .catch((errors) => {
+          console.log(errors.response);
+          this.errors = errors.response;
+        });
+    },
+
+    showInfo: function (product) {
+      console.log("more info...");
+      console.log(product);
+      this.currentProduct = product;
+      document.querySelector("#product-details").showModal();
     },
   },
 };
